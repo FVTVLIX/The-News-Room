ced5268d82af020ba36ad1f8e5378e27c5d96ad1
FVTVLIX <andman290@gmail.com>
2020-04-27 17:09:16 -0400

    linked the API

--- the-news-room/src/App.js
+++ the-news-room/src/App.js
@@ -1,24 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios'
 
 function App() {
+
+  const [articles, updateArticles] = useState([])
+
+  useEffect(() => {
+    const apiCall = async () => {
+      const response = await axios('https://newsapi.org/v2/everything?domains=wsj.com,nytimes.com,cnn.com,engadget.com,espn.com,msn.com,usnews.com,nbcnews.com&apiKey=f038a48516ee4a818e27c24b219789f4')
+
+      updateArticles(response.data)
+      console.log(response)
+
+    }
+    apiCall()
+  }, [])
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
