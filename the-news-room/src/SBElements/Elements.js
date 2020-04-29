777f06fc219f2896af82111fca94489090498f74
FVTVLIX <andman290@gmail.com>
2020-04-29 16:11:09 -0400

    completed a few storybook componenents

---
+++ the-news-room/src/SBElements/Elements.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import "./Elements.css"
+
+const Elements = (props) => (
+    <button style={props.style}>
+    {props.label}
+      <div className='dollar'></div>
+    </button>
+)
+
+export default Elements; 
